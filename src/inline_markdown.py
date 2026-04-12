968f74e60c4311380b26d86a1af7e59e4c722bd1
Amir Zahirovic <amirzahirovic0@gmail.com>
2026-04-13 01:15:52 +0200

    CH3: Text to Textnodes and some fixes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -40,16 +40,17 @@ def extract_markdown_links(text):
 
 def split_nodes_image(old_nodes):
     result_list = []
-
     for node in old_nodes:
-
         extracted_images = extract_markdown_images(node.text)
-        
+
+        if node.text_type != TextType.PLAIN_TEXT:
+            result_list.append(node)
+            continue
+
         if len(extracted_images) == 0:
             result_list.append(node)
-            return result_list
+            continue
         
-        split_nodes = []
         temp_text = node.text
 
         for i in range(len(extracted_images)):
@@ -57,30 +58,31 @@ def split_nodes_image(old_nodes):
             sections = temp_text.split(f"![{image_alt}]({image_url})")
             if len(sections) != 2:
                 raise Exception("invalid markdown: syntax invalid, closing image section not found")
-            if sections[0] == "":
-                split_nodes.append(TextNode(image_alt, TextType.IMAGES, image_url))
-                continue
-            split_nodes.append(TextNode(sections[0], TextType.PLAIN_TEXT))
-            split_nodes.append(TextNode(image_alt, TextType.IMAGES, image_url))
-            if len(sections) > 1:
-                temp_text = sections[1]
+            if sections[0] != "":
+                result_list.append(TextNode(sections[0], TextType.PLAIN_TEXT))
+            result_list.append(TextNode(image_alt, TextType.IMAGES, image_url))
+            
+            temp_text = sections[1]
 
-        result_list.extend(split_nodes)
+        if temp_text != "":
+            result_list.append(TextNode(temp_text, TextType.PLAIN_TEXT))
+
 
     return result_list
 
 def split_nodes_link(old_nodes):
     result_list = []
-
     for node in old_nodes:
-
         extracted_links = extract_markdown_links(node.text)
         
+        if node.text_type != TextType.PLAIN_TEXT:
+            result_list.append(node)
+            continue
+    
         if len(extracted_links) == 0:
             result_list.append(node)
-            return result_list
+            continue
         
-        split_nodes = []
         temp_text = node.text
 
         for i in range(len(extracted_links)):
@@ -88,24 +90,29 @@ def split_nodes_link(old_nodes):
             sections = temp_text.split(f"[{link_alt}]({link_url})")
             if len(sections) != 2:
                 raise Exception("invalid markdown: syntax invalid, closing link section not found")
-            if sections[0] == "":
-                split_nodes.append(TextNode(link_alt, TextType.LINKS, link_url))
-                continue
-            split_nodes.append(TextNode(sections[0], TextType.PLAIN_TEXT))
-            split_nodes.append(TextNode(link_alt, TextType.LINKS, link_url))
-            if len(sections) > 1:
-                temp_text = sections[1]
+            if sections[0] != "":
+                result_list.append(TextNode(sections[0], TextType.PLAIN_TEXT))            
+            result_list.append(TextNode(link_alt, TextType.LINKS, link_url))
+            
+            temp_text = sections[1]
 
-        result_list.extend(split_nodes)
+        if temp_text != "":
+            result_list.append(TextNode(temp_text, TextType.PLAIN_TEXT))
 
     return result_list
 
-        
+def text_to_textnodes(text):
+    result_list = split_nodes_delimiter([TextNode(text, TextType.PLAIN_TEXT)], "_", TextType.ITALIC_TEXT)
+    result_list = split_nodes_delimiter(result_list, "**", TextType.BOLD_TEXT)
+    result_list = split_nodes_delimiter(result_list, "`", TextType.CODE_TEXT)
 
-        
+
+    result_list = split_nodes_image(result_list)
+    result_list = split_nodes_link(result_list)
+
+    return result_list
 
 
-    pass
 
         
 
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -5,6 +5,7 @@ from inline_markdown import extract_markdown_images
 from inline_markdown import extract_markdown_links
 from inline_markdown import split_nodes_image
 from inline_markdown import split_nodes_link
+from inline_markdown import text_to_textnodes
 
 from textnode import TextNode, TextType
 
@@ -92,6 +93,26 @@ class TestMarkdown(unittest.TestCase):
                 ),
             ],
             new_nodes,
+        )
+
+    def test_text_to_textnode(self):
+        sample_text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(sample_text)
+
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.PLAIN_TEXT),
+                TextNode("text", TextType.BOLD_TEXT),
+                TextNode(" with an ", TextType.PLAIN_TEXT),
+                TextNode("italic", TextType.ITALIC_TEXT),
+                TextNode(" word and a ", TextType.PLAIN_TEXT),
+                TextNode("code block", TextType.CODE_TEXT),
+                TextNode(" and an ", TextType.PLAIN_TEXT),
+                TextNode("obi wan image", TextType.IMAGES, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.PLAIN_TEXT),
+                TextNode("link", TextType.LINKS, "https://boot.dev"),
+            ],
+            new_nodes
         )
 
         
