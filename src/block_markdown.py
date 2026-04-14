d5fdc222bcd646404e6f82bbd9fb0ffbe9f08790
Amir Zahirovic <amirzahirovic0@gmail.com>
2026-04-14 12:45:08 +0200

    CH4: Block Types

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,14 @@
+from enum import Enum
+import re
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     blocks_list = markdown.split("\n\n")
 
@@ -10,3 +21,50 @@ def markdown_to_blocks(markdown):
         filtered_blocks.append(block)
 
     return filtered_blocks
+
+def block_to_block_type(block):
+    
+    #HEADERS
+    if block.startswith(("#","##", "###", "####", "#####", "######")):
+        return BlockType.HEADING
+    #CODE BLOCK
+    if block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+    
+    lines = block.split("\n")
+
+    #QUOTE
+    if lines[0].startswith((">", "> ")):
+        result = True
+        for line in lines:
+            if line.startswith((">", "> ")) is False:
+                result = False
+        if result:
+            return BlockType.QUOTE
+        
+    #UNORDERED LIST
+    if lines[0].startswith("- "):
+        result = True
+        for line in lines:
+            if line.startswith("- ") is False:
+                result = False
+        if result:
+            return BlockType.UNORDERED_LIST
+    
+    #ORDERED LIST
+    ordered_list_result = False
+    for i in range(len(lines)):
+        if lines[i].startswith(f"{i + 1}. "):
+            ordered_list_result = True
+        else:
+            return BlockType.PARAGRAPH
+    
+    if ordered_list_result:
+        return BlockType.ORDERED_LIST
+    
+    #PARAGRAPH 
+    
+        
+
+
+
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,8 @@
 import unittest
 
 from block_markdown import markdown_to_blocks
+from block_markdown import block_to_block_type
+from block_markdown import BlockType
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -45,3 +47,48 @@ This is the same paragraph on a new line
                     "- This is a list\n- with items",
                 ],
             )
+
+    def test_md_to_block_type(self):
+        md = """
+This is **bolded** paragraph
+
+
+
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+```
+this
+is a code
+block
+```
+
+> With a quote
+>following
+
+- Just a simple ordered list
+- So we can test that out too
+
+1. And an ordered 
+2. List to
+3. Round it all up
+
+"""        
+        blocks = markdown_to_blocks(md)
+        #print(f"DEBUG:{blocks}")
+        block_types = []
+        for block in blocks:
+            block_types.append(block_to_block_type(block))
+        
+        self.assertListEqual(
+             block_types,
+             [
+                  BlockType.PARAGRAPH,
+                  BlockType.PARAGRAPH,
+                  BlockType.CODE,
+                  BlockType.QUOTE,
+                  BlockType.UNORDERED_LIST,
+                  BlockType.ORDERED_LIST,
+             ]
+        )
