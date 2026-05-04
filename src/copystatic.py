755c483610ff9f5659c3e52273adb05fbf3f4435
Amir Zahirovic <amirzahirovic0@gmail.com>
2026-05-05 01:26:30 +0200

    CH5: L1-Copy Static

---
+++ src/copystatic.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+def copy_static_to_public(source_dir, dest_dir):
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+        os.mkdir(dest_dir)
+    else:
+        os.mkdir(dest_dir)
+    if not os.path.exists(source_dir):
+        raise Exception("source directory does not exist")
+    
+    source_content = os.listdir(source_dir)
+    if len(source_content) == 0:
+        raise Exception("source directory is empty")
+    
+    for i in source_content:
+        source_path = os.path.join(source_dir, i)
+        dest_path = os.path.join(dest_dir, i)
+        if os.path.isfile(source_path):
+            print(f"- {source_path} -> {dest_path}")
+            shutil.copy(source_path, dest_path)
+        else:
+            copy_static_to_public(source_path, dest_path)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
-from textnode import TextNode, TextType
+from copystatic import copy_static_to_public
+
+static_dir_path = "./static"
+public_dir_path = "./public"
+
 
 def main():
-    print(TextNode("This is a dummy text", TextType.BOLD_TEXT))
+    print(f"Copying static files to public directory ...")
+    copy_static_to_public(static_dir_path,public_dir_path)
 
 main()
 
